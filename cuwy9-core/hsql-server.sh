167f7da471eb6d44b7edfb469fd3cd5f7e9ec598
romanm <roman.mishchenko@gmail.com>
2013-06-23 05:57:55 +0200

    new camunda commit

--- cuwy9-core/hsql-server.sh
+++ cuwy9-core/hsql-server.sh
@@ -1,2 +1,2 @@
-java -cp  ~/1d/TASCLIN/lib/hsqldb.jar org.hsqldb.Server -database.0 file:~/1d/DB-java/cuwy7 -dbname.0 cuwy7
+java -cp  ~/1d/1_TASCLIN/lib/hsqldb.jar org.hsqldb.Server -database.0 file:~/1d/DB-java/cuwy7 -dbname.0 cuwy7
 
